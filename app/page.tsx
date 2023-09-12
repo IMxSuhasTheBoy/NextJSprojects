c948bcf3d9af8dc9ff76452a606f3639b1e746da
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-12 17:25:37 +0530

    SSR & CSR componets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import productCard from "./components/ProductCard";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
       <Link href="/users">Users</Link>
+      <ProductCard/>
     </main>
   );
 }
